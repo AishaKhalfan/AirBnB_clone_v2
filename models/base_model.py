d42a1c4fc893c1a78e9d980464e0aa1df10a8fc1
AishaK <aishakhalfan45@gmail.com>
2023-03-22 15:30:15 +0000

    Updating the Base_Model (delete and to_dict)

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from os import getenv
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DATETIME, MetaData
-from models import storage_type
+from sqlalchemy import Column, String, DateTime
+import models
+# from models import storage_type
 
-mymetadata = MetaData()
+#mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
+
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -43,9 +47,9 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -61,7 +65,5 @@ class BaseModel:
 
     def delete(self):
         """deletes the current instance from the dictionary"""
-        from models import storage
-        #key = f"{self.__class__.name__}.{self.id}"
-        storage.delete(self)
-        
+        #from models import storage
+        models.storage.delete(self)
