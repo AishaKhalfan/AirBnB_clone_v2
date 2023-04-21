d6819d7174f15ec8500a72893308d43c096bf67e
AishaK <aishakhalfan45@gmail.com>
2023-04-21 19:31:19 +0000

    Improve engines

--- models/state2.py
+++ models/state2.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""state Module for HBNB project """
+""" State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from models import storage_type
 from models.city import City
-from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    if storage_type == 'db':
+    if models.storage_type == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
@@ -18,11 +20,17 @@ class State(BaseModel, Base):
     else:
         name = ''
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-            '''
+            """returns the list of City instances with state_id
+            equals the current State.id
+            """
             from models import storage
             related_cities = []
             cities = storage.all(City)
