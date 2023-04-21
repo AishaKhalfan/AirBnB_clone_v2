1e4fc75ca490482e1243da99115994034dd3fe1e
AishaK <aishakhalfan45@gmail.com>
2023-04-21 08:12:18 +0000

    a script that starts a Flask web application:

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def c(text):
     """Displays'C' followed by <text> value.
     Replaces any underscores in <text> with slashes.
     """
-    return f"C {text.replace('_', ' ')}"
+    return "C" + text.replace('_', ' ')
 
 
 @app.route("/python", strict_slashes=False)
