f80006c14131a68202b5e57947d12bdc4be8c4a8
AishaK <aishakhalfan45@gmail.com>
2023-04-21 06:27:39 +0000

    a script that starts a Flask web application:

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,15 +14,18 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays'HBNB'"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays'C' followed by <text> value."""
     return f"C {text.replace('_',' ')}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
