4382f153f5a8cd8514eb66cdf320ee71ca01aba4
AishaK <aishakhalfan45@gmail.com>
2023-04-21 08:23:34 +0000

    a script that starts a Flask web application:

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,7 +25,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays'C' followed by <text> value."""
-    return 'C' + text.replace("_", " ")
+    return 'C ' + text.replace("_", " ")
 
 
 if __name__ == "__main__":
