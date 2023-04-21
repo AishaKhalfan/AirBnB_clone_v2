60802c5ef79064b327b78e5ebbb754af4ba7a3de
AishaK <aishakhalfan45@gmail.com>
2023-04-21 06:35:21 +0000

    a script that starts a Flask web application:

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -14,10 +14,12 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays'HBNB'"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
@@ -25,6 +27,7 @@ def c(text):
     Replaces any underscores in <text> with slashes.
     """
     return f"C {text.replace('_',' ')}"
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -34,6 +37,7 @@ def python(text="is cool"):
     """
     text = text.replace("_"," ")
     return "Python {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
