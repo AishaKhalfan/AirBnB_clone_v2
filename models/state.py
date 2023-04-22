4a55f59ceb8b0dcefe08fab932a1fb65f963aef8
AishaK <aishakhalfan45@gmail.com>
2023-04-22 14:05:45 +0000

    Improve engines

--- models/state.py
+++ models/state.py
@@ -20,8 +20,7 @@ class State(BaseModel, Base):
     else:
         name = ''
 
-        #if getenv("HBNB_TYPE_STORAGE") != "db":
-
+        # if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             '''returns the list of City instances with state_id
