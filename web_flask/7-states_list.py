c0efc2511da4b8b3f3878d3afba1dce16257c70e
AishaK <aishakhalfan45@gmail.com>
2023-04-22 20:48:46 +0000

    Engines

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,8 +17,8 @@ def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
         States are sorted by name
     """
-    states = storage.all(State)
-    return render_template('states_list.html', states=states)
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
@@ -28,4 +28,4 @@ def teardown_db(exception=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
