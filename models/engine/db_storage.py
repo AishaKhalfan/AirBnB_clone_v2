c66654f0cf0c48c18db220b8b4b1f8b984a45f69
AishaK <aishakhalfan45@gmail.com>
2023-04-22 14:41:33 +0000

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.amenity import Amenity
-from models.base_model import Base, BaseModel
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
