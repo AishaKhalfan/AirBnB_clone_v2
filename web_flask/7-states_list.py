ed05fb043765d5f64a1541c4d6a87f559276fbfd
AishaK <aishakhalfan45@gmail.com>
2023-04-24 13:30:48 +0000

    Module Documentation for 7-states.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,8 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all state objects in DBStorage.
 """
+
+
 from models import storage
 from flask import Flask, render_template
 from models.state import State
